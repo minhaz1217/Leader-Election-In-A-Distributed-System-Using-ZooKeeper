e2e0fefdb9362177d5e89a422ef53852561ac7cb
minhazul hayat khan <minhaz1217@gmail.com>
2022-05-02 23:55:16 +0600

    initial commit

---
+++ Leader Election In A Distributed System Using ZooKeeper/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Leader_Election_In_A_Distributed_System_Using_ZooKeeper
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
